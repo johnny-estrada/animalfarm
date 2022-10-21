ed2cb271e486359182478a72b9438c43c8d2c98d
Johnny Estrada <johnnye562@yahoo.com>
2022-10-21 11:32:00 -0700

    Add search UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery);
+  }, [])
+
+  const search = async (q) => {
+    const response = await fetch(
+      "http://localhost:8080?" + new URLSearchParams({ q })
+    );
+
+    const data = await response.json();
+
+    setAnimals(data);
+    localStorage.getItem('lastQuery', q)
+  };
 
   return (
-    <div className="App">
-      
-    </div>
-  )
+    <main>
+      <h1>Animal Farm</h1>
+
+      <input
+        type="text"
+        placeholder="search"
+        onChange={(e) => search(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+
+        {animals.length === 0 && "No animals found"}
+      </ul>
+    </main>
+  );
 }
 
-export default App
+function Animal({ type, name, age }) {
+  return (
+    <li>
+      <strong>{type}</strong> {name} ({age} years old)
+    </li>
+  );
+}
+
+export default App;
